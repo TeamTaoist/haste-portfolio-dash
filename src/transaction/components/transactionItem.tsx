9b617186676cf8f5f068c9525f23cd4eb858fdc7
lee920217 <francislee920217@gmail.com>
2024-03-29 00:16:04 +0800

    update explorer

--- src/transaction/components/transactionItem.tsx
+++ src/transaction/components/transactionItem.tsx
@@ -56,7 +56,7 @@ export function TransactionItem() {
             <CardContent>
               <div className="font-SourceSanPro text-left flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#080808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
-                <a href={`https://pudge.explorer.nervos.org/${tx.txHash}`} target="_href">{tx.txHash}</a>
+                <a href={`https://pudge.explorer.nervos.org/transaction/${tx.txHash}`} target="_href">{tx.txHash}</a>
               </div>
             </CardContent>
           </Card>
