f3171fcef6f665015f5862c783bf3236f28a3889
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-04-07 20:18:58 +0800

    update udt table

--- src/app/_components/Dashboard/udt.tsx
+++ src/app/_components/Dashboard/udt.tsx
@@ -1,36 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
 export default function UDTList() {
+  const [tokens] = useState(new Array(100).fill(0));
+
   return (
-    <div className="mt-4 w-full">
+    <div className="w-full">
       <table className="w-full">
         <thead>
-          <tr className="font-SourceSanPro font-semibold text-primary003 text-right lg:text-left grid grid-cols-12 py-2">
+          <tr className="font-SourceSanPro font-semibold text-primary003 grid grid-cols-12 py-2 text-left">
             <th className="col-span-7 lg:col-span-5 cursor-pointer px-2">
               Token
             </th>
-            <th className="text-right lg:text-left col-span-4 lg:col-span-2 cursor-pointer px-2">
+            <th className="col-span-4 lg:col-span-2 cursor-pointer px-2">
               Balance
             </th>
           </tr>
         </thead>
         <tbody className="text-white">
-          <tr className="hover:bg-primary010 grid grid-cols-12 group py-2 border-t border-gray-500">
-            <td className="col-span-7 lg:col-span-5 px-2">SUDT</td>
-            <td className="px-2 whitespace-nowrap sm:w-auto col-span-3 lg:col-span-2">
-              111
-            </td>
-          </tr>
-          <tr className="hover:bg-primary010 grid grid-cols-12 group py-2 border-t border-gray-500">
-            <td className="col-span-7 lg:col-span-5 px-2">SUDT</td>
-            <td className="px-2 whitespace-nowrap sm:w-auto col-span-3 lg:col-span-2">
-              111
-            </td>
-          </tr>
-          <tr className="hover:bg-primary010 grid grid-cols-12 group py-2 border-t border-gray-500">
-            <td className="col-span-7 lg:col-span-5 px-2">SUDT</td>
-            <td className="px-2 whitespace-nowrap sm:w-auto col-span-3 lg:col-span-2">
-              111
-            </td>
-          </tr>
+          {tokens.map((_, index) => (
+            <tr
+              key={index}
+              className="hover:bg-primary010 grid grid-cols-12 group py-2 border-t border-gray-500"
+            >
+              <td className="col-span-7 lg:col-span-5 px-2">SUDT</td>
+              <td className="px-2 whitespace-nowrap sm:w-auto col-span-3 lg:col-span-2">
+                0.000001
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
