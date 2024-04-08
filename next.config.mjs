aa1ecc39153cfc7d3eec768870fde02a86b90db4
Frozen <1884084+xrdavies@users.noreply.github.com>
2024-04-09 02:33:22 +0800

    update config to remove error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['static.cx.metamask.io'],
+    remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "static.cx.metamask.io", // if your website has no www, drop it
+    },
+    {
+      protocol: "http",
+      hostname: "localhost",
+    }],
   },
   webpack(config, { nextRuntime }) { 
     // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
