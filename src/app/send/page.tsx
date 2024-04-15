c3cbaf1baa6081b35ee7ca1ac194d5c25c13cd6a
lee920217 <francislee920217@gmail.com>
2024-04-15 17:29:24 +0800

    update page tsx

--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
+import React, { useState } from "react";
 import SendContent from "@/app/_components/SendComponent/send";
 import ReceiveContent from "@/app/_components/SendComponent/receive";
 
-enum TABVALUE {
-  SEND = "send",
-  RECEIVE = "receive",
-}
 
-const Send = () => {
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("tab") as TABVALUE;
-  const [currentTab, setCurrentTab] = useState<TABVALUE>();
-
-  useEffect(() => {
-    setCurrentTab(
-      [TABVALUE.SEND, TABVALUE.RECEIVE].includes(tab) ? tab : TABVALUE.SEND
-    );
-  }, [tab]);
+const Send: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState<'send' | 'receive'>('send');
 
   return (
     <main className="flex flex-col flex-1 min-h-screen bg-gray-100 text-black h-full">
@@ -31,8 +17,10 @@ const Send = () => {
         <div className="flex items-center justify-center w-full flex-1 flex-grow">
           <div className="rounded-2xl bg-white p-4  px-4 sm:px-6 h-auto w-[420px] sm:w-full py-8">
             <div className="flex -mx-4 top-0 overflow-hidden sm:space-x-0 sm:px-7 bg-inherit  border-none z-1 static px-4">
-              <Link
-                href="/send?tab=send"
+              <div
+                onClick={() => {
+                  setCurrentTab('send')
+                }}
                 className={`${
                   currentTab === "receive"
                     ? ""
@@ -40,9 +28,11 @@ const Send = () => {
                 } p-2 mx-2 font-medium relative text-default focus:outline-none focus:ring-0 py-0 pb-2  w-auto sm:w-[116px] px-0 sm:mx-0 cursor-pointer text-left`}
               >
                 Send
-              </Link>
-              <Link
-                href="/send?tab=receive"
+              </div>
+              <div
+                onClick={() => {
+                  setCurrentTab('receive')
+                }}
                 className={`${
                   currentTab === "receive"
                     ? "activeTab text-primary011 font-Montserrat"
@@ -50,10 +40,10 @@ const Send = () => {
                 } p-2 mx-2 font-medium relative text-default focus:outline-none focus:ring-0 w-auto py-0 pb-2 sm:w-[116px] px-0 sm:mx-0 cursor-pointer text-left`}
               >
                 Receive
-              </Link>
+              </div>
             </div>
             <div className="flex flex-col gap-6 w-full pt-6 pb-2 px-2">
-              {tab === TABVALUE.RECEIVE ? <ReceiveContent /> : <SendContent />}
+              {currentTab === 'receive' ? <ReceiveContent /> : <SendContent />}
             </div>
           </div>
         </div>
